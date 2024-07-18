d12a3682ac4d689b09d4605a9aa899317a00166a
jstgrowup <deysubham999@gmail.com>
2024-07-18 11:03:28 +0530

    working on the mongo assignment

---
+++ week-3/03-mongo/db/index.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://admin:9e76OXmkeoPn1pG6@cluster0.ffa4elf.mongodb.net/"
+);
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+  // Schema definition here
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserSchema = new mongoose.Schema({
+  // Schema definition here
+});
+
+const CourseSchema = new mongoose.Schema({
+  // Schema definition here
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageLink: {
+    type: String,
+    required: true,
+  },
+});
+
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
+
+module.exports = {
+  Admin,
+  User,
+  Course,
+};
--- week-4/react-foundation/react-intro/src/App.jsx
+++ week-4/react-foundation/react-intro/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 import "./App.css";
-
 function App() {
   const [count, setCount] = useState(0);
   return (
@@ -12,5 +11,4 @@ function App() {
     </>
   );
 }
-
 export default App;
