5a1156ee1275a446e3c96c49b56cb6643f7d4ac8
jstgrowup <deysubham999@gmail.com>
2024-07-27 00:58:18 +0530

    use memo

--- week-6/class/src/App.jsx
+++ week-6/class/src/App.jsx
@@ -1,6 +1,22 @@
+import { useState, memo } from "react";
 import "./App.css";
 function App() {
-  return <></>;
+  const [title, settitle] = useState("my name is subham");
+  function changeTitle() {
+    settitle("my name is " + Math.random());
+  }
+  return (
+    <div>
+      <button onClick={changeTitle}>Click me to change the title</button>
+      <Header title={title} />
+      <Header title={"My name is subham"} />
+      <Header title={"My name is subham"} />
+      <Header title={"My name is subham"} />
+      <Header title={"My name is subham"} />
+    </div>
+  );
 }
-
+const Header = memo(function ({ title }) {
+  return <div>{title}</div>;
+});
 export default App;
