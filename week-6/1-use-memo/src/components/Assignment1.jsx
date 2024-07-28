deda8cb09cb66f1d5ae8aca38c5571635e05c6ba
jstgrowup <deysubham999@gmail.com>
2024-07-28 08:41:37 +0530

    hooks

---
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input.
+// Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
+function factorial(n) {
+  if (n < 0) return -1;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
+export function Assignment1() {
+  const [input, setInput] = useState(0);
+  // Your solution starts here
+
+  // Your solution ends here
+  const expensiveValue = useMemo(() => factorial(input), [input]);
+  return (
+    <div>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(Number(e.target.value))}
+      />
+      <p>Calculated Value: {expensiveValue}</p>
+    </div>
+  );
+}
--- week-6/class/src/App.jsx
+++ week-6/class/src/App.jsx
@@ -1,22 +1,7 @@
 import { useState, memo } from "react";
 import "./App.css";
 function App() {
-  const [title, settitle] = useState("my name is subham");
-  function changeTitle() {
-    settitle("my name is " + Math.random());
-  }
-  return (
-    <div>
-      <button onClick={changeTitle}>Click me to change the title</button>
-      <Header title={title} />
-      <Header title={"My name is subham"} />
-      <Header title={"My name is subham"} />
-      <Header title={"My name is subham"} />
-      <Header title={"My name is subham"} />
-    </div>
-  );
+  return <div></div>;
 }
-const Header = memo(function ({ title }) {
-  return <div>{title}</div>;
-});
+
 export default App;
