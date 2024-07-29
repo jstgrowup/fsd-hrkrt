8d5ced6f7ee0cb19889f7013260c8b435421b8ff
jstgrowup <deysubham999@gmail.com>
2024-07-29 23:43:26 +0530

    starting with recoil

--- week-7/class/src/App.jsx
+++ week-7/class/src/App.jsx
@@ -1,9 +1,50 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext, useState } from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { CountContext } from "./context";
 
 function App() {
-  return <div></div>;
+  const [count, setcount] = useState(0);
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+        <Count setcount={setcount} />
+      </CountContext.Provider>
+    </div>
+  );
 }
+const Count = ({ setcount }) => {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons setCount={setcount} />
+    </div>
+  );
+};
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return <div>{count}</div>;
+}
+function Buttons({ setCount }) {
+  const count = useContext(CountContext);
 
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
 export default App;
---
+++ week-7/class/src/context.jsx
@@ -0,0 +1,7 @@
+import React, { createContext } from "react";
+export const CountContext = createContext(0);
+const Context = () => {
+  return <div>Context</div>;
+};
+
+export default Context;
