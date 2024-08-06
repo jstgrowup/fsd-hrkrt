354a27eb4d523c3de2a7f10c1054d51d54daa847
jstgrowup <deysubham999@gmail.com>
2024-08-06 23:35:40 +0530

    starting with frontend

--- week-8/8.2(paytm)/frontend/src/App.jsx
+++ week-8/8.2(paytm)/frontend/src/App.jsx
@@ -1,11 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./component/Signup";
+import SignIn from "./component/SignIn";
+import Dashboard from "./component/Dashboard";
+import Send from "./component/Send";
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Send />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
