1d86a80033fd3c77be9ab58e73c3d7a25debe303
jstgrowup <deysubham999@gmail.com>
2024-08-06 21:07:39 +0530

    learning transaction

--- week-8/8.2(paytm)/backend/routes/accounts.route.js
+++ week-8/8.2(paytm)/backend/routes/accounts.route.js
@@ -6,6 +6,8 @@ const {
   creditHelper,
   getBalanceByUser,
 } = require("../helpers/accounts.helpers");
+const { startSession } = require("mongoose");
+const Account = require("../db/accounts.schema");
 accountRouter.post("/create-transaction", async (req, res) => {
   const transactionBody = req.body;
   const validatedUser = transferZodSchema.safeParse(transactionBody);
@@ -36,11 +38,44 @@ accountRouter.get("/balance", async (req, res) => {
   if (!balanceDetails) {
     return res
       .status(411)
-      .json({ message: "Failed to fetch the account balancec" });
+      .json({ message: "Failed to fetch the account balance" });
   }
   return res.status(200).json({
     message: "Account Balance fetched successfully",
     data: balanceDetails,
+  });
+});
+accountRouter.get("/transfer", async (req, res) => {
+  const session = await startSession();
+
+  session.startTransaction();
+  const { amount, to } = req.body;
+  const userId = req.userId;
+  const account = await Account.findOne({ userId }).session(session);
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
+  const creditAccount = await Account.findOne({ userId: to }).session(session);
+  if (!creditAccount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Invalid account",
+    });
+  }
+  await Account.updateOne(
+    { userId: req.userId },
+    { $inc: { balance: -amount } }
+  ).session(session);
+  await Account.updateOne(
+    { userId: to },
+    { $inc: { balance: amount } }
+  ).session(session);
+  await session.commitTransaction();
+  return res.status(200).json({
+    message: "Transfer successful",
   });
 });
 module.exports = accountRouter;
