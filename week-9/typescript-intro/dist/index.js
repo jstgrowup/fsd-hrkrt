b7d2c32ca3d6dbc596ec939b31f2d9e1874b357b
jstgrowup <deysubham999@gmail.com>
2024-08-07 23:16:32 +0530

    learning typescript

---
+++ week-9/typescript-intro/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const x = 1;
+console.log(x);
+function Name(firstName) {
+    console.log("hello" + firstName);
+}
+Name("fsdfsdf");
---
+++ week-9/typescript-intro/src/index.ts
@@ -0,0 +1,6 @@
+const x: number = 1;
+console.log(x);
+function Name(firstName: string) {
+  console.log("hello" + firstName);
+}
+Name(" ");
