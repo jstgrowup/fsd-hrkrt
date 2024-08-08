f3aca20e858db57c3be9643a1a37bc4136a7f4e5
jstgrowup <deysubham999@gmail.com>
2024-08-08 20:35:24 +0530

    almost done with the basic typescirpt

--- week-9/typescript-intro/src/index.ts
+++ week-9/typescript-intro/src/index.ts
@@ -4,3 +4,28 @@ function Name(firstName: string) {
   console.log("hello" + firstName);
 }
 Name(" ");
+interface Manager {
+  name: string;
+}
+type Techlead = {
+  time: string;
+};
+// interface Both=Manager& Techlead
+// above is not possible
+type Both = Manager & Techlead;
+const bot: Both = {
+  name: "yaay",
+  time: "huru",
+};
+// type Both = Manager | Techlead;
+// const bot: Both = {
+//   name: "yaay",
+// };
+function generic<T>(arg: T): T {
+  return arg;
+}
+const op1 = generic<string>("my string");
+console.log("op1:", op1);
+const op2 = generic<number>(3);
+console.log("op2:", op2);
+op1.toUpperCase();
