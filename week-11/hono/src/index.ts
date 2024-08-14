b1b7b1d9c2cd89173611783f3e9599e807ed4bb9
jstgrowup <deysubham999@gmail.com>
2024-08-14 10:36:36 +0530

    working with hono

---
+++ week-11/hono/src/index.ts
@@ -0,0 +1,21 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+async function authMiddleware(c: any, next: any) {
+  // c is context of this request , response
+  if (c.req.header("Authorization")) {
+    await next();
+  } else {
+    return c.text("Unauthorized");
+  }
+}
+
+app.get("/", async (c) => {
+  const body = await c.req.json();
+  const authHeader = c.req.header("Authorization");
+  const cookie = c.req.header("Cookie");
+  const param = c.req.query("param");
+  return c.text("Hello Hono!yoyo");
+});
+
+export default app;
