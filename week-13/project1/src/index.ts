b4012e517416f39025da5ce813d2542747b1134c
jstgrowup <deysubham999@gmail.com>
2024-08-21 23:41:54 +0530

    getting started with hono

--- week-13/project1/src/index.ts
+++ week-13/project1/src/index.ts
@@ -5,5 +5,21 @@ const app = new Hono();
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+app.post("/api/vi/signup", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/api/vi/signin", (c) => {
+  return c.text("Hello Hono!");
+});
+app
+  .post("/api/vi/blog", (c) => {
+    return c.text("Hello Hono!");
+  })
+  .put((c) => {
+    return c.text("Hello Hono!");
+  });
 
+app.get("/api/vi/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
 export default app;
