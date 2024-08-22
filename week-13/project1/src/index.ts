5d918d83854e8b3b10ffc8dbae4713ba28a49629
jstgrowup <deysubham999@gmail.com>
2024-08-22 11:11:52 +0530

    signup route using Hono and prisma

--- week-13/project1/src/index.ts
+++ week-13/project1/src/index.ts
@@ -1,12 +1,30 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
 app.get("/", (c) => {
+  // context ahs all the things like request and response
   return c.text("Hello Hono!");
 });
-app.post("/api/vi/signup", (c) => {
-  return c.text("Hello Hono!");
+app.post("/api/vi/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c?.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password,
+    },
+  });
+  return c.json(newUser);
 });
 app.post("/api/vi/signin", (c) => {
   return c.text("Hello Hono!");
