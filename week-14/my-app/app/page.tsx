68a8231134f946afd448760c73cb57ed55fc5977
jstgrowup <deysubham999@gmail.com>
2024-09-04 23:59:03 +0530

    adding some notes

--- week-14/my-app/app/page.tsx
+++ week-14/my-app/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>yuhu</div>;
+import axios from "axios";
+
+async function getUserData() {
+  await new Promise((r) => setTimeout(r, 4000));
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+  return response.data;
+}
+// it will call teh api in the server itself
+export default async function Home() {
+  const userDetails = await getUserData();
+  return (
+    <div>
+      {userDetails.email}
+      {userDetails.name}
+    </div>
+  );
 }
--- week-14/my-app/components/Signup-from.tsx
+++ week-14/my-app/components/Signup-from.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { AUTH_TYPE } from "@/utils/enums/Global-enums";
 import { useState } from "react";
 import LabelledInput from "./Labelled-input";
@@ -5,6 +6,11 @@ import AuthHeader from "./Auth-header";
 import AuthButton from "./Auth-button";
 
 const SignupForm = () => {
+  const [userInputs, setuserInput] = useState<any>({
+    email: "",
+    name: "",
+    password: "",
+  });
   return (
     <div className="h-screen flex justify-center flex-col items-center">
       <AuthHeader type={AUTH_TYPE.SIGNUP} />
@@ -12,24 +18,24 @@ const SignupForm = () => {
         <LabelledInput
           label="Email"
           placeholder="Enter your email"
-          //   onChange={(e) =>
-          //     setuserInput({ ...userInputs, email: e.target.value })
-          //   }
+          onChange={(e) =>
+            setuserInput({ ...userInputs, email: e.target.value })
+          }
         />
         <LabelledInput
           label="Name"
           placeholder="Enter your name"
-          //   onChange={(e) =>
-          //     setuserInput({ ...userInputs, name: e.target.value })
-          //   }
+          onChange={(e) =>
+            setuserInput({ ...userInputs, name: e.target.value })
+          }
         />
         <LabelledInput
           label="Password"
           placeholder="Enter your password"
           type="password"
-          //   onChange={(e) =>
-          //     setuserInput({ ...userInputs, password: e.target.value })
-          //   }
+          onChange={(e) =>
+            setuserInput({ ...userInputs, password: e.target.value })
+          }
         />
         <AuthButton type={AUTH_TYPE.SIGNUP} />
       </div>
