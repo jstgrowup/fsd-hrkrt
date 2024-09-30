d97d07fdd1ac6d02518c036baf313ce0e3c5a64f
jstgrowup <deysubham999@gmail.com>
2024-09-30 09:36:45 +0530

    hdfc webhook server

--- week-17/apps/bank-webhook/src/index.ts
+++ week-17/apps/bank-webhook/src/index.ts
@@ -1,10 +1,42 @@
 import express from "express";
 const app = express();
 import db from "@repo/db/client";
-app.post("/hdfc/payment", (req, res) => {
-  const paymentInformation = {
-    token: req.body.token,
-    userId: req.body.user_identifier,
-    amount: req.body.amount,
-  };
+app.post("/hdfc/payment", async (req, res) => {
+  try {
+    const paymentInformation = {
+      token: req.body.token,
+      userId: req.body.user_identifier,
+      amount: req.body.amount,
+    };
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInformation.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInformation.amount,
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+    res.json({
+      message: "Captured the payment",
+    });
+  } catch (error) {
+    res.status(411).json({
+      message: "Something went wrong while capturing the transaction",
+    });
+  }
+});
+app.listen(3003, () => {
+  console.log("webhook server up and running at 3003");
 });
