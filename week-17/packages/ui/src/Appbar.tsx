cb2256400420881de7410f2bc3ed68688534e59b
jstgrowup <deysubham999@gmail.com>
2024-10-02 00:08:21 +0530

    making the auth form

--- week-17/packages/ui/src/Appbar.tsx
+++ week-17/packages/ui/src/Appbar.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { Button } from "./button";
-
+import { User, LogOut } from "lucide-react";
 interface AppbarProps {
   user?: {
     name?: string | null;
@@ -12,15 +11,55 @@ interface AppbarProps {
 export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex justify-between border-b px-4">
-      <div className="w-40 border text">
-        <div className="text-lg flex flex-col justify-center">PayTM</div>
-        <div className="flex flex-col justify-center pt-2">
-          <Button onClick={user ? onSignout : onSignin}>
-            {user ? "Logout" : "Login"}
-          </Button>
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-28">
+          <div className="flex justify-center items-center">
+            <img
+              src="https://pwebassets.paytm.com/commonwebassets/paytmweb/header/images/logo.svg"
+              alt="paytm"
+              className="w-52"
+            />
+          </div>
+          <div className="hidden sm:ml-6 sm:flex sm:items-center">
+            <button
+              onClick={user ? onSignout : onSignin}
+              className="rounded-xl bg-blue-500 hover:bg-blue-600 text-white text-md font-medium px-14 py-3"
+            >
+              {user ? "Logout" : "Login"}
+            </button>
+
+            <div className="ml-3 relative">
+              {user && (
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  className="flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  <span className="sr-only">Open user menu</span>
+                  <User className="h-8 w-8 rounded-full" />
+                </button>
+              )}
+
+              {isOpen && (
+                <div className="rounded origin-top-right absolute right-0 mt-2 w-48  shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    <User className="inline-block mr-2 h-5 w-5" /> Profile
+                  </a>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    <LogOut className="inline-block mr-2 h-5 w-5" /> Sign out
+                  </a>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
--- week-17/packages/ui/tailwind.config.ts
+++ week-17/packages/ui/tailwind.config.ts
@@ -1,44 +1,60 @@
-import type { Config } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
-
 const config = {
-  darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "../../packages/ui/src/**/*.{ts,tsx}",
-  ],
-  prefix: "ui-",
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
-        myColor: "#a16207",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        primary: {
+          "50": "#eff6ff",
+          "100": "#dbeafe",
+          "200": "#bfdbfe",
+          "300": "#93c5fd",
+          "400": "#60a5fa",
+          "500": "#3b82f6",
+          "600": "#2563eb",
+          "700": "#1d4ed8",
+          "800": "#1e40af",
+          "900": "#1e3a8a",
+          "950": "#172554",
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    fontFamily: {
+      body: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      sans: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
   },
-  plugins: [tailwindcssAnimate],
-} satisfies Config;
+  plugins: [],
+};
 export default config;
