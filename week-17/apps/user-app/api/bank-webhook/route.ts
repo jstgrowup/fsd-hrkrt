d049ba6860a8f8458848fca2f2effc879bac5aea
jstgrowup <deysubham999@gmail.com>
2024-10-16 10:22:55 +0530

    starting with the cron

--- week-17/apps/user-app/api/bank-webhook/route.ts
+++ week-17/apps/user-app/api/bank-webhook/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-const db = require("@repo/db/client");
+import db from "@repo/db/client";
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
