b6650c9c0cfb289bfea92c362644454b720ed572
jstgrowup <deysubham999@gmail.com>
2025-01-07 10:07:08 +0530

    minor fixes

--- week-19/worker/src/index.ts
+++ week-19/worker/src/index.ts
@@ -1,19 +1,21 @@
-import express from "express";
 import { createClient } from "redis";
-const app = express();
-app.use(express.json());
-const client = createClient({ url: "redis://localhost:6379" });
+
+const client = createClient();
 async function main() {
-  await client.connect();
-  while (1) {
-    const response = await client.brPop("submissions", 0);
-    console.log("response:in the brpop", response);
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        resolve;
-      }, 1000)
-    );
-    console.log("Processed users submissions");
+  try {
+    await client.connect();
+    while (1) {
+      const response = await client.brPop("submissions", 0);
+      console.log("response:in the brpop", response);
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve;
+        }, 1000)
+      );
+      console.log("Processed users submissions");
+    }
+  } catch (error) {
+    console.log("error:to connect to redis", error);
   }
 }
 main();
